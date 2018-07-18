d07ead4916a4d3dcdadc445fb3fd886e793dfb4f
Enrico Scapin <me@escapin.net>
2018-07-19 00:25:19 +0200

    createDataZplug

--- createDataZplug/matchingZplugWattInterval.m
+++ createDataZplug/matchingZplugWattInterval.m
@@ -1,30 +1,33 @@
 function [ minZplug maxZplug avgZplug ] = matchingZplugWattInterval(ZplugActive, timeline, operation)
 %
-%    function res = matchingSensorInterval(sensActive, timeline)
+%    function [ minZplug maxZplug avgZplug ] = matchingSensorInterval(sensActive, timeline)
 %
-% Per ogni intervallo temporale della timeline assegna 1 sse il sensore è attivo in quel determinato intervallo
+% For each timestamp 't_i' in the timeline, we set the value to 1 if, 
+% in the time interval [t_i,t_(i-1)], the Zplug has been 
+% activated at least once.
+%
 %
 % INPUTS:
 %   - ZplugActive: cell 2xN containging the Watt power and the relevation instant
 %               1,j Watt value
 %               2,j relevation instant
-%   - timeline: 1xN array containging some time instants
+%   - timeline: 1xN array of timestamps
 %   - operation: which operation made ok every batch
 % OUTPUT:
 %   - res: 1xN cell where res(i)=1 iff sensor is active in i-th temporal interval, 0 otherwise.
 
-lenght = size(timeline);
+lenghtTimeline = size(timeline);
 lenghtTable=size(ZplugActive);
 
-minZplug = zeros(1,lenght(2));
-maxZplug = zeros(1,lenght(2));
-avgZplug = zeros(1,lenght(2));
+minZplug = zeros(1,lenghtTimeline(2));
+maxZplug = zeros(1,lenghtTimeline(2));
+avgZplug = zeros(1,lenghtTimeline(2));
 
 % to set up the first interval
 startTimeline=addtodate(datenum(timeline(1,1)), -3, 'minute');
 endTimeline=datenum(timeline(1,1));
 nextJ=1;
-for i=1:lenght(2)
+for i=1:lenghtTimeline(2)
     endInnerLoop=0;
     j=nextJ; % to avoid to check the previous intervals
     batch=zeros(90,1);
@@ -35,15 +38,18 @@ for i=1:lenght(2)
             nextJ = j+1;
         elseif (endTimeline < instant) % to avoid to check the next intervals
             endInnerLoop=1;
+            %disp('endTimeline'); timeline(1,i)
+            %disp('instant'); ZplugActive(2,j)
+            %disp('--------------');
         else
 %             i
 %             j
 %             k
 %             size(batch(k,1))
-%             size(cell2num(ZplugActive(1,j)))
+%             size(cell2mat(ZplugActive(1,j)))
 %             size(k)
-            %disp('ciao')
-            batch(k)=str2double(cell2num(ZplugActive(1,j)));
+%            disp('--------------------------------');
+            batch(k)=str2double(cell2mat(ZplugActive(1,j)));
             k=k+1;
         end
         j=j+1;
@@ -52,15 +58,19 @@ for i=1:lenght(2)
         minZplug(i)=min(batch(1:k-1));
         maxZplug(i)=max(batch(1:k-1));
         avgZplug(i)=mean(batch(1:k-1));
+        
     else
         minZplug(i)=1;
         maxZplug(i)=1;
         avgZplug(i)=1;
     end
     startTimeline=datenum(timeline(1,i));
-    if(i~=lenght(2)) % to avoid Out Of Bound Error
+    if(i~=lenghtTimeline(2)) % to avoid Out Of Bound Error
+        %i
+        %lenghtTimeline(2)
+        %disp('-------');
         endTimeline=datenum(timeline(1,i+1));
     end
 end
-res=num2cell(res);
+%res=num2cell(res);
 end
--- createDataZplug/unionZplug.m
+++ createDataZplug/unionZplug.m
@@ -1,16 +1,21 @@
 function [ zPlug ] = unionZplug(database, startDate, endDate)
 
 % addpath(genpathKPM(pwd))
-mksqlite('open', database);
+%mksqlite('open', database);
+dbid=sqlite(fullfile(pwd, database));
 
-queryZplug = ['SELECT Parameter_State.ID, Parameter_State.Value, Parameter_State.Time FROM Parameter_State ' ... 
+queryZplug = ['SELECT Parameter_State.Value, Parameter_State.Time FROM Parameter_State ' ... 
 ' JOIN Parameter ON Parameter_State.FK_Parameter=Parameter.ID WHERE Parameter.Name=''potenza'' AND ' ...
 ' Parameter_State.Time BETWEEN Datetime(''%s'') AND Datetime(''%s'')' ...
 ' ORDER BY Parameter_State.Time'];
 
 queryZplug=sprintf(queryZplug, startDate, endDate);
 
-zPlug =  mksqlite(queryZplug);
+%zPlug = mksqlite(queryZplug);
+zPlug =  fetch(dbid, queryZplug);
 
+zPlug = rot90(zPlug);% for backward compatability
+zPlug = flipud(zPlug);
 
-mksqlite('close');
+% mksqlite('close');
+close(dbid);
