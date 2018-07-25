83e9fc98add0e58d30e2ddd9d742003b022182f9
Enrico Scapin <escapin@users.noreply.github.com>
2018-07-25 19:30:05 +0200

    Update loadBNTlibrary.m

--- loadBNTlibrary.m
+++ loadBNTlibrary.m
@@ -1,9 +1,5 @@
-% Script loading the 'bnt' library to the workspace
+% Script loading the libraries of the Bayesian Network Toolbox to the workspace
 
-
-% add the library to query the SQLite db
-addpath(genpath('lib/mksqlite'));
-% add the libraries of the Bayesian Network Toolbox
 cd lib/bnt
 addpath(genpathKPM(pwd));
 cd BNT
